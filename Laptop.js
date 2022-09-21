d7fd4551c5c3a506f6380e1a4e91db7890a5b73a
SephoraRose8511 <84019245+SephoraRose8511@users.noreply.github.com>
2022-09-21 15:32:46 +0530

    Add files via upload

---
+++ Laptop.js
@@ -0,0 +1,61 @@
+function setup() 
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(640, 420);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+img = "" ;
+Status = "" ;
+
+function preload()
+{
+    img = loadImage("Laptop.jpg");
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    Status = true;
+}
+objects = [];
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+ {
+    image(img, 0, 0, 640, 420);
+
+    if(Status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(img, gotResult);
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected : " + objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
+ }
